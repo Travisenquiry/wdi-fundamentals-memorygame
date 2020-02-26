26474caf438d0ef7b60f5f728d15841ab208e107
Travis Lim <travisenquiry@gmail.com>
2020-02-25 20:38:08 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-let cardOne = "queen"
-let cardTwo = "queen"
-let cardThree = "king"
-let cardFour = "king"
+const cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
 
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardTwo);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped" + " " + cardsInPlay);
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else{
+		alert("Sorry, try again.");
+	}
+}
